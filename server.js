df9506f10b3184d05de98713584718ee90a65a9b
pagella42 <pagella42@gmail.com>
2019-09-16 17:35:09 -0500

    deploy2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const api = require('./server/routes/api')
 
 app.use(expressip().getIpInfoMiddleware);
 
-mongoose.connect("mongodb://localhost/weather")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weather")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -22,9 +22,6 @@ app.use(express.static(path.join(__dirname,  'dist')))
 app.use(express.static(path.join(__dirname,  'node_modules')))
 app.use('/', api)
 
-
-
-app.listen(2500, function() {
-    console.log("Server up and running on port 2500")
-  })
+const PORT = 8080
+app.listen(process.env.PORT || PORT);
   
